dfd38850dcfa0760a4345de46ec15253e63b15b4
Mporo <polinamporo@gmail.com>
2026-03-05 12:27:47 +0300

    Add fonts, cteating blobs, ticket-title

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    window.addEventListener("resize", (e) => {
+        let ticket = document.querySelector('[data-test="circles"]')
+        let blobWidth = 50
+        let paddings = parseInt(window.getComputedStyle(ticket).getPropertyValue("padding-left")) * 2;
+        let gap = parseInt(window.getComputedStyle(ticket).getPropertyValue("row-gap"));
+        html = ``;
+        for (let i = 0; i < Math.floor((e.currentTarget.innerWidth - parseInt(paddings)) / (blobWidth + gap)); i++) {
+            html += `<div class="blob"></div>`;
+        }
+        ticket.innerHTML = html;
+    })
 })
